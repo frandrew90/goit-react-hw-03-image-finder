8ab223edd3e4b6a1df955cd3e1601b81b6ce35e7
frandrew90 <frandrewjs@gmail.com>
2021-10-03 14:41:02 +0300

    Updates

---
+++ static/js/main.2965ca67.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);n(1);var r=n(3),c=n.n(r),i=n(0),o=function(){return Object(i.jsx)("h2",{children:"Hello"})};n(9);c.a.render(Object(i.jsx)(i.Fragment,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.2965ca67.chunk.js.map
