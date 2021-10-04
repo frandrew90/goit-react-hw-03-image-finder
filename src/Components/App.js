4622afff26e557336bf6eb058bf2229fd6d05a94
frandrew90 <frandrewjs@gmail.com>
2021-10-04 18:01:12 +0300

    fixed structure of project

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,7 +4,7 @@ import { getPhoto } from '../services/ApiServices';
 import ImageGallery from './imageGallery/ImageGallery';
 import Modal from './modal/Modal';
 import Button from './button/Button';
-import Loader from '../loader/Loader';
+import Loader from './loader/Loader';
 
 class App extends Component {
   state = {
